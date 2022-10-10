7a8a89c85ed4942da5152e39204f34429f666d44
Abhiraj <195514@nith.ac.in>
2022-10-10 14:39:48 +0530

    Created new class to handle crud operations

--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import *
-
+from rest_framework import generics
 
 # we cannot use the html here as we are using rest framework which returns json data
 
@@ -112,136 +112,149 @@ def get_book(request):
 #             'message': 'invalid id'
 #         })
 
-class StudentAPI(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        student_obj = Student.objects.all()
-        serializer = StudentSerializer(student_obj, many=True)
-        return Response({
-            'status': 200,
-            'data': serializer.data,
-            'message': 'data successfully fetched'
-        })
-
-    def patch(self, request):
-        try:
-            #                                    id from front end
-            student_obj = Student.objects.get(id=request.data['id'])
-            serializer = StudentSerializer(
-                student_obj, data=request.data, partial=True)
-
-            if serializer.is_valid():
-                serializer.save()
-                return Response({
-                    'status': 200,
-                    'data': serializer.data,
-                    'message': 'Data successfully updated'
-                })
-            else:
-                print(serializer.errors)
-                return Response({
-                    'status': 404,
-                    'error': serializer.errors,
-                    'message': 'you send the wrong data'
-                })
-        except Exception as e:
-            return Response({
-                'status': 404,
-                'error': e,
-                'message': 'invalid id'
-            })
-
-    def delete(self, request):
-        try:
-            student_obj = Student.objects.get(id=request.data['id'])
-            student_obj.delete()
-            return Response({
-                'status': 200,
-                'message': 'Deleted successfully',
-            })
-        except Exception as e:
-            return Response({
-                'status': 404,
-                'error': e,
-                'message': 'invalid id'
-            })
-
-    def put(self, request):
-        try:
-            student_obj = Student.objects.get(id=request.data['id'])
-            serializer = StudentSerializer(student_obj, data=request.data)
-
-            if serializer.is_valid():
-                serializer.save()
-                return Response({
-                    'status': 200,
-                    'data': serializer.data,
-                    'message': 'data successfully updated'
-                })
-            else:
-                print(serializer.errors)
-                return Response({
-                    'status': 404,
-                    'error': serializer.errors,
-                    'message': 'you send the wrong data'
-                })
-        except Exception as e:
-            return Response({
-                'status': 404,
-                'error': e,
-                'message': 'invalid id'
-            })
-
-
-class RegisterUser(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            user = User.objects.get(username=serializer.data['username'])
-            refresh = RefreshToken.for_user(user)
+# class StudentAPI(APIView):
+#     authentication_classes = [JWTAuthentication]
+#     permission_classes = [IsAuthenticated]
+#
+#     def get(self, request):
+#         student_obj = Student.objects.all()
+#         serializer = StudentSerializer(student_obj, many=True)
+#         return Response({
+#             'status': 200,
+#             'data': serializer.data,
+#             'message': 'data successfully fetched'
+#         })
+#
+#     def patch(self, request):
+#         try:
+#             #                                    id from front end
+#             student_obj = Student.objects.get(id=request.data['id'])
+#             serializer = StudentSerializer(
+#                 student_obj, data=request.data, partial=True)
+#
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response({
+#                     'status': 200,
+#                     'data': serializer.data,
+#                     'message': 'Data successfully updated'
+#                 })
+#             else:
+#                 print(serializer.errors)
+#                 return Response({
+#                     'status': 404,
+#                     'error': serializer.errors,
+#                     'message': 'you send the wrong data'
+#                 })
+#         except Exception as e:
+#             return Response({
+#                 'status': 404,
+#                 'error': e,
+#                 'message': 'invalid id'
+#             })
+#
+#     def delete(self, request):
+#         try:
+#             student_obj = Student.objects.get(id=request.data['id'])
+#             student_obj.delete()
+#             return Response({
+#                 'status': 200,
+#                 'message': 'Deleted successfully',
+#             })
+#         except Exception as e:
+#             return Response({
+#                 'status': 404,
+#                 'error': e,
+#                 'message': 'invalid id'
+#             })
+#
+#     def put(self, request):
+#         try:
+#             student_obj = Student.objects.get(id=request.data['id'])
+#             serializer = StudentSerializer(student_obj, data=request.data)
+#
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response({
+#                     'status': 200,
+#                     'data': serializer.data,
+#                     'message': 'data successfully updated'
+#                 })
+#             else:
+#                 print(serializer.errors)
+#                 return Response({
+#                     'status': 404,
+#                     'error': serializer.errors,
+#                     'message': 'you send the wrong data'
+#                 })
+#         except Exception as e:
+#             return Response({
+#                 'status': 404,
+#                 'error': e,
+#                 'message': 'invalid id'
+#             })
 
 
-            return Response({
-                'status': 200,
-                'data': serializer.data,
-                'message': 'user successfully created',
-                'refresh': str(refresh),
-                'access': str(refresh.access_token)
-            })
-        else:
-            return Response({
-                'status': 404,
-                'error': serializer.errors,
-                'message': 'Try with different username'
-            })
+# class RegisterUser(APIView):
+#     def post(self, request):
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             user = User.objects.get(username=serializer.data['username'])
+#             refresh = RefreshToken.for_user(user)
+#
+#
+#             return Response({
+#                 'status': 200,
+#                 'data': serializer.data,
+#                 'message': 'user successfully created',
+#                 'refresh': str(refresh),
+#                 'access': str(refresh.access_token)
+#             })
+#         else:
+#             return Response({
+#                 'status': 404,
+#                 'error': serializer.errors,
+#                 'message': 'Try with different username'
+#             })
+#
+#     SIMPLE_JWT = {
+#         'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+#         'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+#         'ROTATE_REFRESH_TOKENS': False,
+#         'BLACKLIST_AFTER_ROTATION': True,
+#         'UPDATE_LAST_LOGIN': False,
+#
+#         'ALGORITHM': 'HS256',
+#         'SIGNING_KEY': settings.SECRET_KEY,
+#         'VERIFYING_KEY': None,
+#         'AUDIENCE': None,
+#         'ISSUER': None,
+#
+#         'AUTH_HEADER_TYPES': ('Bearer',),
+#         'USER_ID_FIELD': 'id',
+#         'USER_ID_CLAIM': 'user_id',
+#         'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+#
+#         'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+#         'TOKEN_TYPE_CLAIM': 'token_type',
+#
+#         'JTI_CLAIM': 'jti',
+#
+#         'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+#         'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+#         'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+#     }
 
-    SIMPLE_JWT = {
-        'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-        'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-        'ROTATE_REFRESH_TOKENS': False,
-        'BLACKLIST_AFTER_ROTATION': True,
-        'UPDATE_LAST_LOGIN': False,
 
-        'ALGORITHM': 'HS256',
-        'SIGNING_KEY': settings.SECRET_KEY,
-        'VERIFYING_KEY': None,
-        'AUDIENCE': None,
-        'ISSUER': None,
+# Post and get method
+class StudentGeneric(generics.ListAPIView, generics.CreateAPIView):
+    queryset = Student.objects.all();
+    serializer_class = StudentSerializer
 
-        'AUTH_HEADER_TYPES': ('Bearer',),
-        'USER_ID_FIELD': 'id',
-        'USER_ID_CLAIM': 'user_id',
-        'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+# Update and delete method
 
-        'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-        'TOKEN_TYPE_CLAIM': 'token_type',
-
-        'JTI_CLAIM': 'jti',
-
-        'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-        'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-        'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-    }
+class StudentGeneric1(generics.UpdateAPIView, generics.DestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    lookup_field = 'id' # for handing the id passed in the url
